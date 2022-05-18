92453a0fc9d2890b24286876f9cb7c9b59797318
nyaaaaaaaaaaaaaa <103323641+nyaaaaaaaaaaaaaa@users.noreply.github.com>
2022-05-18 14:13:48 +0000

    [Add] controllerファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
+
+  resources :books, only: [:new, :create, :index, :show]
+  #get 'books/new'
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'homes/about' => 'homes#about', as: 'about'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_15_075602) do
+ActiveRecord::Schema.define(version: 2022_05_18_134207) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
